b5eafb137814a8b146b0d559d4d0f176b02d92f8
Spannakin <spannakin@gmail.com>
2021-05-20 06:35:42 +0000

    clear layout for variable choices

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,7 @@
+//---Level Choice
 const easyGame = docment.getElementsByClassName(easy-game);
 const medGame = document.getElementsByClassName(med-game);
 const hardGame = document.getElementsByClassName(hard-game);
+
+//---Game set up--//
+const question = document.getElementsByClassName(question);
