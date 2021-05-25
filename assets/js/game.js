33e9c372868500251a34fc00bba2412389d955d8
Spannakin <spannakin@gmail.com>
2021-05-25 08:49:39 +0000

    function for screen navigation added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,41 @@
 //---Game Screen setup---//
+let screenSelect = document.getElementsByClassName(game-select-page);
+let welcomeScreen = document.getElementById(home);
+let gameScreen = document.getElementById(game);
+let levelSelect = document.getElementById(level-select)
+let howToPlay = document.getElementById(how-to-play)
 
+function toWelcomeScreen() {
+    welcomeScreen.classList.remove("hide");
+    gameScreen.classList.add("hide");
+    levelSelect.classList.add("hide");
+    howToPlay.classList.add("hide");
+    screenSelectMovement();
+}
+
+function toGameScreen() {
+    welcomeScreen.classList.add("hide");
+    gameScreen.classList.remove("hide");
+    levelSelect.classList.add("hide");
+    howToPlay.classList.add("hide");
+    screenSelectMovement();
+}
+
+function toLevelSelectScreen() {
+    welcomeScreen.classList.add("hide");
+    gameScreen.classList.add("hide");
+    levelSelect.classList.remove("hide");
+    howToPlay.classList.add("hide");
+    screenSelectMovement();
+}
+
+function toHowToPlayScreen() {
+    welcomeScreen.classList.add("hide");
+    gameScreen.classList.add("hide");
+    levelSelect.classList.add("hide");
+    howToPlay.classList.remove("hide");
+    screenSelectMovement();
+    
 //---Level Choice
 const easyGame = docment.getElementsByClassName(easy-game);
 const medGame = document.getElementsByClassName(med-game);
