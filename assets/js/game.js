35af9fab581e98f74c85e5d24abcf039d48fc49b
Spannakin <spannakin@gmail.com>
2021-05-25 07:17:51 +0000

    skeleton for functions to be created added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,18 @@
+//---Game Screen setup---//
+
 //---Level Choice
 const easyGame = docment.getElementsByClassName(easy-game);
 const medGame = document.getElementsByClassName(med-game);
 const hardGame = document.getElementsByClassName(hard-game);
 
-//---Game set up--//
+//---Game Question Setup--//
 const question = document.getElementsByClassName(question);
+
+//---Answer selection---//
+
+//---Correct/Incorrect Answer---//
+
+//---Score Tracker---//
+
+//---Results---//
+
