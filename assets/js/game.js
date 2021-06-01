76d4f50b0a46285081be3ff3a518a011382f4d9b
Spannakin <spannakin@gmail.com>
2021-06-01 06:42:56 +0000

    js started again for screen movemtn functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,51 +1,25 @@
 //---Game Screen setup---//
 //---Screens--//
-let welcomeScreen = document.getElementById(home);
-let levelSelect = document.getElementById(level-select);
-let howToPlay = document.getElementById(how-to-play);
-let gameScreen = document.getElementById(game);
+var levelScreen = document.getElementById("level-select");
+var gameScreen = document.getElementById("game");
+var homeScreen = document.getElementsByClass("home");
 //--Buttons--//
-let levelButton = document.getElementsByClassName(level-select-button);
-let howToButton = document.getElementsByClassName(instructions-button);
+
 
 //---Event listeners to move between screens---//
-
-levelButton.addEventListner('click'() => {
-    welcomeScreen.classList.add('hide');
-    levelSelect.classList.remove('hide');
-}); 
-
-
-howToButton.addEventListner('click'() => {
-    welcomeScreen.classList.add('hide');
-    howToPlay.classList.remove('hide');
-});
-
-
-/** Home screen 
-    gameScreen.classList.add("hide");
-    levelSelect.classList.add("hide");
-    howToPlay.classList.add("hide");
-    screenSelectMovement();
-*/
+function levelScreenMovement(event) {
+    levelScreen.classList.remove('hide');
+    homeScreen.classList.add('hide');
+};
+let levelButton = document.getElementById("level-select-button");
+levelButton.addEventListener('click', levelScreenMovement)
     
 /**Level Choice**/
 /* each chose should load question bank and lead to game screen */
 
-const easyGame = docment.getElementsByClassName(easy-game);
-const medGame = document.getElementsByClassName(med-game);
-const hardGame = document.getElementsByClassName(hard-game);
-
-function toGameScreen() {
-    welcomeScreen.classList.add("hide");
-    gameScreen.classList.remove("hide");
-    levelSelect.classList.add("hide");
-    howToPlay.classList.add("hide");
-    screenSelectMovement();
-}
 
 //---Game Question Setup--//
-const question = document.getElementsByClassName(question);
+
 
 //---Answer selection---//
 
